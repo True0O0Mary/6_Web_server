ef7ea6bff54ec0ae6e2fb1fcd838f907f2974176
True0O0Mary <truefalsemary@gmail.com>
2022-12-14 18:20:46 +0300

    Done 5 task (log in file) + attemp to do 7

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket
 import os
-import threading
 from settings import HOST, PORT, WORKING_DIR, REQUEST_SIZE
 from check import code_request
 from datetime import datetime
@@ -62,31 +61,3 @@ if __name__ == "__main__":
             t.start()
             sleep(2)
 
-
-# while True:
-#
-#     conn, addr = server.accept()
-#     print("Connected", addr)
-#
-#     request = conn.recv(8192).decode().split('\n')
-#
-#     method, url, protocol = request[0].split(' ')
-#     print(url)
-#     url = os.path.join(WORKING_DIR, url[1:])
-#     print(url)
-#
-#     code = "404 Not Found"
-#     body = ""
-#     url = os.path.join(url, 'index.htm')
-#     if os.path.isfile(url):
-#         code = "200 OK"
-#         body = open(url, 'r').read()
-#         print(f"Body: {body}")
-#
-#     resp = f'HTTP/1.1 {code}\n'
-#     resp += "Server: my_dummy_server"
-#     resp += '\n\n'
-#     resp += body
-#     conn.send(resp.encode())
-#     conn.close()
-#     print('Connection closed\n')
--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ import os
 
 
 HOST = "10.0.2.15"
-PORT = 80
+PORT = 5000
 WORKING_DIR = os.getcwd()
 REQUEST_SIZE = 25240
